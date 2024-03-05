a1a6bf72efc7d546705084fd1445b89c54592c6c
jeffavanti <160894402+jeffavanti@users.noreply.github.com>
2024-03-05 12:04:42 -0700

    Update new_user.sh

--- new_user.sh
+++ new_user.sh
@@ -1,67 +1,78 @@
 #!/bin/bash
 
 # Variables
-DSCL="/usr/bin/dscl"
-MKDIR="/bin/mkdir"
+new="/usr/bin/dscl"
+dir="/bin/mkdir"
+reboot="/sbin/reboot"
 
-# Function to display error and exit
-display_error_and_exit() {
+# Function to handle errors
+handle_error() {
     echo "Error: $1"
     exit 1
 }
 
-# Get user input
-read -p "Enter user's login name: " username
-read -p "Enter user's Display Name: " display_name
+# Function to create user
+create_user() {
+    echo "Creating user..."
+    if ! sudo $new . -create "$user"; then
+        handle_error "Could not create user."
+    fi
 
-# User-specific variables
+    echo "Setting RealName..."
+    if ! sudo $new . -create "$user" RealName "$name"; then
+        handle_error "Could not set RealName."
+    fi
+
+    echo "Setting password..."
+    if ! sudo $new . -passwd "$user" "$(echo $username | awk '{print toupper(substr($0,1,1))tolower(substr($0,2))}')$(date +%y)"; then
+        handle_error "Could not set password."
+    fi
+
+    echo "Setting user shell..."
+    if ! sudo $new . -create "$user" UserShell /bin/bash; then
+        handle_error "Could not set user shell."
+    fi
+
+    echo "Setting NFSHomeDirectory..."
+    if ! sudo $new . -create "$user" NFSHomeDirectory "/Users/$user"; then
+        handle_error "Could not set NFSHomeDirectory."
+    fi
+}
+
+# Function to create user's home directory
+create_user_directory() {
+    echo "Creating user's home directory..."
+    if ! sudo $dir -p "$user/Desktop/Remote Share"; then
+        handle_error "Could not create user's home directory."
+    fi
+}
+
+# Main script
+echo "Enter user's login name"
+read username
+
+echo "Enter user's Display Name"
+read display_name
+
 user="/Users/$username"
 name="$display_name"
-remote_share_dir="$user/Desktop/Remote Share"
 
 echo "This computer will be assigned to $name"
 
-# Account creation
-echo "Creating user..."
-if ! sudo $DSCL . -create "$user"; then
-    display_error_and_exit "Could not create user."
-fi
-
-echo "Setting RealName..."
-if ! sudo $DSCL . -create "$user" RealName "$name"; then
-    display_error_and_exit "Could not set RealName."
-fi
-
-echo "Setting password..."
-if ! sudo $DSCL . -passwd "$user" "$(echo $username | awk '{print toupper(substr($0,1,1))tolower(substr($0,2))}')$(date +%y)"; then
-    display_error_and_exit "Could not set password."
-fi
-
-echo "Setting user shell..."
-if ! sudo $DSCL . -create "$user" UserShell /bin/bash; then
-    display_error_and_exit "Could not set user shell."
-fi
-
-echo "Setting NFSHomeDirectory..."
-if ! sudo $DSCL . -create "$user" NFSHomeDirectory "/Users/$user"; then
-    display_error_and_exit "Could not set NFSHomeDirectory."
-fi
-
-# Create user's home directory
-echo "Creating user's home directory..."
-if ! sudo $MKDIR "$remote_share_dir"; then
-    display_error_and_exit "Could not create user's home directory."
-fi
+create_user
+create_user_directory
 
 # Set new name for computer
 sudo /usr/sbin/scutil --set HostName "$username"
 
 # Admin rights
-read -p "Does the user need admin privileges? (yes/no): " userInput
+echo "Does the user need admin privileges? (yes/no)"
+read userInput
+
 if [ "$userInput" == "yes" ]; then
     echo "Granting admin privileges to user..."
-    if ! sudo $DSCL . -append /Groups/admin GroupMembership "$user"; then
-        display_error_and_exit "Could not grant admin privileges."
+    if ! sudo $new . -append /Groups/admin GroupMembership "$user"; then
+        handle_error "Could not grant admin privileges."
     fi
 
     echo "Admin privileges granted to user: $user"
@@ -70,10 +81,5 @@ else
 fi
 
 # Reboot computer
-read -p "Do you want to reboot the computer now? (yes/no): " rebootInput
-if [ "$rebootInput" == "yes" ]; then
-    echo "Rebooting computer..."
-    sudo /sbin/reboot
-else
-    echo "Script completed. You may need to reboot the computer manually for changes to take effect."
-fi
+echo "Rebooting computer..."
+sudo $reboot

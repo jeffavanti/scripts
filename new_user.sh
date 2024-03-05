1cd6686a06144cc5f2b71a30610e32bb681c86db
jeffavanti <160894402+jeffavanti@users.noreply.github.com>
2024-03-05 10:42:17 -0700

    Add files via upload

---
+++ new_user.sh
@@ -0,0 +1,45 @@
+#!/bin/sh   
+
+# Variables
+new=/usr/bin/dscl
+dir=/bin/mkdir
+
+# Get name for local account creation
+echo "Enter user's login name"
+read username
+
+echo "Enter user's Display Name"
+read display_name
+
+# User input
+user="/Users/$username"
+name="$display_name"
+
+echo "This computer will be assigned to $name"
+
+# Account creation
+$new . -create "$user"
+$new . -create "$user" RealName "$name"
+$new . -passwd "$user" "$(echo ${user^})$(date +%y)"
+$new . -create "$user" UserShell /bin/bash
+$new . -create "$user" NFSHomeDirectory "/Local/Users/$username"
+$dir "/Users/$user/Desktop/Remote Share"
+
+# Set new name for computer
+/usr/sbin/scutil --set Hostname "$username"
+
+# Admin rights
+echo "Does the user need admin privileges? (yes/no)"
+read userInput
+
+if [ "$userInput" == "yes" ]; then
+    echo "Granting admin privileges to user..." 
+    $new . -append /Groups/admin GroupMembership "$user"
+
+    echo "Admin privileges granted to user: $user"
+else
+    echo "No admin privileges will be granted to $user."
+fi
+
+# Reboot computer
+/sbin/reboot
